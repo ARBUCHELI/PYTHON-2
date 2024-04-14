193ce2979f58313fe67b35c060fbcd34d44d4039
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-14 21:31:41 +0300

    conditionals and control flow - conditionals and control flow - if you are having

---
+++ Conditionals and Control Flow/Conditionals and Control Flow/if-you-are-having/script.py
@@ -0,0 +1,10 @@
+def using_control_once():
+    if 2 * 3 == 6:
+        return "Success #1"
+
+def using_control_again():
+    if "dog" == "dog":
+        return "Success #2"
+
+print (using_control_once())
+print (using_control_again())
